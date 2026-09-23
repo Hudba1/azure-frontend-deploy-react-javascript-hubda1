8a23ee3bfa45b14d6a3ee5717eba3d91fd588089
Sander Sandvik Nessa <sander.nessa@no.experis.com>
2026-09-23 11:18:43 +0200

    feat: header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h1>Hi!</h1>
+    <h2>Hello {value}
+    </h2>
+  </div>;
 }
 
 export default App;
